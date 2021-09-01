cd26f4c186eb92e09363d45284a3d694d460d749
rsneal01 <rsneal01@gmail.com>
2021-09-01 10:10:15 -0700

    commit tree d4af5d5beea07c78a9f37b4ddd44c1d208f6af86

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    updateDOM()
+  });
+
+  function updateDOM() {
+    document.getElementById("text")
+    .innerHTML = "This is really cool!";
+  }
+
+
+//   document.addEventListener( "DOMContentLoaded", function () {
+//     updateDOM()
+//   } );
+  
+  
+//   function updateDOM() {
+//     document.getElementById( "text" )
+//       .innerHTML = "This is really cool!";
+//   }
